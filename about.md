da444d509faca3ec257b183484386e06adf8f77e
Kevin Zheng <kevzheng@meta.com>
2025-01-21 17:40:49 +0900

    iter 2

---
+++ about.md
@@ -0,0 +1,10 @@
+---
+layout: profile
+bio_nav: /
+bio_nav_desc: home
+---
+
+### About Me 👋
+I'm currently a software engineer at Meta working on EMG Neuromotor Interfaces. I've worked on deployment on machine learning models onto low-power embedded devices, as well as the infrastructure to help researchers bring ideas into a real product.
+
+Previously, I've worked at Amazon (Tax Engine), Jump Trading, Correlation One (Recruiting for Quant firms), and Meta (Computer Vision / Video OCR)
---
+++ js/dropdown.js
@@ -0,0 +1,30 @@
+var grid = new Muuri('.grid', {dragEnabled: false, layout: {fillGaps: true}});
+
+var filterField = document.querySelector('.filter-field');
+filterField.addEventListener('change', filter);
+
+function filter() {
+  grid.filter(function (item) {
+    var element = item.getElement();
+    var isFilterMatch = filterField.value == "" ? true : (element.getAttribute('data-category').includes(filterField.value))
+  
+    return isFilterMatch;
+  
+  });
+}
+
+function DropDown(el) {
+    this.dd = el;
+    this.initEvents();
+}
+
+DropDown.prototype = {
+  initEvents : function() {
+      var obj = this;
+
+      obj.dd.on('click', function(event){
+          $(this).toggleClass('active');
+          event.stopPropagation();
+      }); 
+  }
+}
